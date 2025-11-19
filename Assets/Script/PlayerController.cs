00d29a929b8b6f968a3eab842df3963f58722d94
MalineeSunrise <malineebukong@gmail.com>
2025-11-19 23:41:11 +0700

    แก้การเดินในห้องนอน

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -1,8 +1,7 @@
-using UnityEngine;
+๏ปฟusing UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [Header("การเคลื่อนที่")]
     public float walkSpeed = 5f;
     public float jumpForce = 10f;
 
@@ -19,35 +18,35 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        // รับ Input
+        
         moveInput = Input.GetAxisRaw("Horizontal");
 
-        // กระโดด
+       
         if (Input.GetKeyDown(KeyCode.Space))
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         }
 
-        // พลิกตัว
+        
         if (moveInput > 0 && !facingRight)
             Flip();
         else if (moveInput < 0 && facingRight)
             Flip();
 
-        // อัพเดท Animation
+        
         UpdateAnimation();
     }
 
     void FixedUpdate()
     {
-        // เคลื่อนที่
+        
         rb.velocity = new Vector2(moveInput * walkSpeed, rb.velocity.y);
     }
 
     void UpdateAnimation()
     {
-        float speed = Mathf.Abs(moveInput); // ถ้า moveInput เป็น 0, speed จะเป็น 0
-        anim.SetFloat("Speed", speed); // ส่งค่า Speed = 0 ไปที่ Animator
+        float speed = Mathf.Abs(moveInput);
+        anim.SetFloat("Speed", speed); 
     }
 
     void Flip()
